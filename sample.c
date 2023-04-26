c7e000bc6a01ee44d1e2f9bf05fddb63e117cb62
Queng07 <131233731+Queng07@users.noreply.github.com>
2023-04-26 14:11:47 +0800

    edited from here
    edited from here

--- sample.c
+++ sample.c
@@ -8,7 +8,7 @@ int main ()
  printf("Hello world!");
  printf("Joshua Balladares Desu");
  printf("Hello, I'm Karl Bejerano");
- printf("Hello from branch staging);
+ printf("Hello from branch staging -edited yeah);
 return0;
 
 }
